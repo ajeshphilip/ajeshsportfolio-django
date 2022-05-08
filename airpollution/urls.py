a53bc5667418e9a3d830f855230fb8b8c5c787b0
ajeshphilip <66029815+ajeshphilip@users.noreply.github.com>
2022-05-08 23:04:40 +0530

    prepare excel headers for upload

--- airpollution/urls.py
+++ airpollution/urls.py
@@ -6,4 +6,5 @@ app_name = 'airpollution'
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('upload_file', views.upload_file, name='upload_file'),
+    path('temp_country_creator', views.temp_country_creator, name='temp_country_creator')
 ]
