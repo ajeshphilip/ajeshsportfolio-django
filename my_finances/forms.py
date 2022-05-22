a6cf9971ca27f697f6c713b4d1cd025dfbcc37ad
ajeshphilip <ajesh.philip@gmail.com>
2022-05-22 22:35:42 +0530

    T54-fully working portfolio

--- my_finances/forms.py
+++ my_finances/forms.py
@@ -15,7 +15,7 @@ class IncomeOutcomeForm(forms.ModelForm):
         repetitive = self.cleaned_data.get('repetitive')
         repetition_interval = self.cleaned_data.get('repetition_interval')
         repetition_time = self.cleaned_data.get('repetition_time')
-        # repetition_end = self.cleaned_data.get('repetition_end')
+        repetition_end = self.cleaned_data.get('repetition_end')
 
         if value <= 0:
             self.add_error('value', 'Value must be a positive number.')
@@ -32,10 +32,10 @@ class IncomeOutcomeForm(forms.ModelForm):
             if repetition_time == 0:
                 self.add_error('repetition_time', 'Repetition time can not be 0 when Repetition is selected.')
                 is_valid = False
-            # if form_date and repetition_end:
-            #     if repetition_end <= form_date:
-            #         self.add_error('repetition_end', 'Repetition end date can\' be before or on the Date.')
-            #         is_valid = False
+            if form_date and repetition_end:
+                if repetition_end <= form_date:
+                    self.add_error('repetition_end', 'Repetition end date can\' be before or on the Date.')
+                    is_valid = False
         else:  # if Repetition is False
             if repetition_interval != 1:
                 self.add_error('repetition_interval', 'Repetitive needs to be selected when Repetition interval is '
@@ -44,16 +44,16 @@ class IncomeOutcomeForm(forms.ModelForm):
             if repetition_time != 0:
                 self.add_error('repetition_time', 'Repetition needs to be selected when Repetition time is no 0.')
                 is_valid = False
-            # if repetition_end:
-            #     self.add_error('repetition_end', 'Repetitive needs to be selected when Repetition end is not empty.')
-            #     is_valid = False
+            if repetition_end:
+                self.add_error('repetition_end', 'Repetitive needs to be selected when Repetition end is not empty.')
+                is_valid = False
 
         return is_valid
 
 class IncomeForm(IncomeOutcomeForm):
     class Meta:
         model = Income
-        fields = ['value', 'date', 'type', 'repetitive', 'repetition_interval', 'repetition_time', 'comment_text']
+        fields = ['value', 'date', 'type', 'repetitive', 'repetition_interval', 'repetition_time', 'repetition_end', 'comment_text']
 
     value = forms.DecimalField(initial=100)
     date = forms.DateField(widget=DateInput, initial=date.today())
@@ -76,7 +76,7 @@ class IncomeForm(IncomeOutcomeForm):
 class OutcomeForm(IncomeOutcomeForm):
     class Meta:
         model = Outcome
-        fields = ['value', 'date', 'type', 'repetitive', 'repetition_interval', 'repetition_time', 'comment_text']
+        fields = ['value', 'date', 'type', 'repetitive', 'repetition_interval', 'repetition_time', 'repetition_end', 'comment_text']
 
     date = forms.DateField(widget=DateInput, initial=date.today())
 
--- my_finances/views.py
+++ my_finances/views.py
@@ -1,20 +1,17 @@
-from django.shortcuts import render
+from datetime import date, timedelta
 from django.contrib import messages
-from django.db.models import Q, Sum, Min, Max
-from django.http import HttpResponseRedirect
+from django.db.models import Sum
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from datetime import date
 
-from my_finances.models import Income, Outcome, Balance
 from my_finances.forms import IncomeForm, OutcomeForm, BalanceForm
 from my_finances.helpers import calculate_repetitive_total
+from my_finances.models import Income, Outcome, Balance
 
-
-# Create your views here.
 
 class IncomeListView(ListView):
     model = Income
@@ -22,9 +19,13 @@ class IncomeListView(ListView):
     template_name = 'my_finances/balance_income_outcome_list.html'
     extra_context = {'list_what': 'Income'}
 
+    # queryset = Income.objects.all()
+
     def get_queryset(self):
         user = self.request.user
         return Income.objects.filter(user=user)
+    # context_object_name = 'income_list'
+    # allow_empty = True
 
 
 class IncomeDetailView(DetailView):
@@ -32,23 +33,36 @@ class IncomeDetailView(DetailView):
     template_name = 'my_finances/balance_income_outcome_detail.html'
     extra_context = {'detail_what': 'Income'}
 
+    # queryset = Income.objects.all()
+
     def get_queryset(self):
         user = self.request.user
         return Income.objects.filter(user=user)
+    # context_object_name = 'income'
 
 
-class IncomeCreateView(CreateView):
+class IncomeCreatView(CreateView):
     model = Income
     form_class = IncomeForm
     template_name = 'my_finances/balance_income_outcome_form.html'
     extra_context = {'header': 'Add Income'}
+
+    # template_name_suffix = '_create_form'
+    # success_url = reverse_lazy('my_finances:income_list')
+
+    # def post(self, request, *args, **kwargs):
+    #     form_date = request.POST['date']
+    #     form_date = datetime.strptime(form_date, '%Y-%m-%d')
+    #     if (form_date - datetime.today()).days < 0:
+    #         messages.error(self.request, 'Income can not be backdated')
+    #         return super().get(request, *args, **kwargs)
+    #     return super().post(request, *args, **kwargs)
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
         self.object.user = self.request.user
         self.object.save()
         return HttpResponseRedirect(self.get_success_url())
-
 
     def get_success_url(self):
         messages.success(self.request, 'Income created successfully!')
@@ -60,11 +74,15 @@ class IncomeUpdateView(UpdateView):
     form_class = IncomeForm
     template_name = 'my_finances/balance_income_outcome_form.html'
     extra_context = {'header': 'Update Income'}
-    success_url = reverse_lazy('my_finances:income_list')
+
+    # template_name_suffix = '_update_form'
+    # queryset = Income.objects.all()
 
     def get_queryset(self):
         user = self.request.user
         return Income.objects.filter(user=user)
+
+    # context_object_name = 'income'
 
     def get_success_url(self):
         messages.success(self.request, 'Income updated successfully!')
@@ -76,9 +94,14 @@ class IncomeDeleteView(DeleteView):
     template_name = 'my_finances/balance_income_outcome_confirm_delete.html'
     extra_context = {'delete_what': 'Income'}
 
+    # template_name_suffix = '_delete_form'
+    # queryset = Income.objects.all()
+
     def get_queryset(self):
         user = self.request.user
         return Income.objects.filter(user=user)
+
+    # context_object_name = 'income'
 
     def get_success_url(self):
         messages.success(self.request, 'Income deleted successfully!')
@@ -106,7 +129,7 @@ class OutcomeDetailView(DetailView):
         return Outcome.objects.filter(user=user)
 
 
-class OutcomeCreateView(CreateView):
+class OutcomeCreatView(CreateView):
     model = Outcome
     form_class = OutcomeForm
     template_name = 'my_finances/balance_income_outcome_form.html'
@@ -127,7 +150,6 @@ class OutcomeUpdateView(UpdateView):
     model = Outcome
     form_class = OutcomeForm
     template_name = 'my_finances/balance_income_outcome_form.html'
-    success_url = reverse_lazy('my_finances:outcome_list')
     extra_context = {'header': 'Update Outcome'}
 
     def get_queryset(self):
@@ -140,7 +162,7 @@ class OutcomeUpdateView(UpdateView):
 
 
 class OutcomeDeleteView(DeleteView):
-    model = Income
+    model = Outcome
     template_name = 'my_finances/balance_income_outcome_confirm_delete.html'
     extra_context = {'delete_what': 'Outcome'}
 
@@ -150,7 +172,7 @@ class OutcomeDeleteView(DeleteView):
 
     def get_success_url(self):
         messages.success(self.request, 'Outcome deleted successfully!')
-        return reverse_lazy('my_finances:balance_list')
+        return reverse_lazy('my_finances:outcome_list')
 
 
 class BalanceListView(ListView):
@@ -174,7 +196,7 @@ class BalanceDetailView(DetailView):
         return Balance.objects.filter(user=user)
 
 
-class BalanceCreateView(CreateView):
+class BalanceCreatView(CreateView):
     model = Balance
     form_class = BalanceForm
     template_name = 'my_finances/balance_income_outcome_form.html'
@@ -195,7 +217,6 @@ class BalanceUpdateView(UpdateView):
     model = Balance
     form_class = BalanceForm
     template_name = 'my_finances/balance_income_outcome_form.html'
-    success_url = reverse_lazy('my_finances:balance_list')
     extra_context = {'header': 'Update Balance'}
 
     def get_queryset(self):
@@ -208,7 +229,7 @@ class BalanceUpdateView(UpdateView):
 
 
 class BalanceDeleteView(DeleteView):
-    model = Income
+    model = Balance
     template_name = 'my_finances/balance_income_outcome_confirm_delete.html'
     extra_context = {'delete_what': 'Balance'}
 
@@ -221,36 +242,191 @@ class BalanceDeleteView(DeleteView):
         return reverse_lazy('my_finances:balance_list')
 
 
-def current_finances(request):
+def current_period(request):
+    make_calls = True
     last_balance = Balance.objects.filter(user=request.user, type=1).order_by('-date').first()
     if not last_balance:
-        messages.warning(request, 'No current balances has been recorded. '
+        messages.warning(request, 'No current balance has been recorded. '
                                   'Please add at least one current balance record.')
-        return render(request, 'my_finances/current_finances.html')
+        make_calls = False
+    return render(request, 'my_finances/current_period.html', context={'make_calls': make_calls})
 
+
+def year_overview(request):
+    make_calls = True
+    last_balance = Balance.objects.filter(user=request.user, type=1).order_by('-date').first()
+    last_savings_balance = Balance.objects.filter(user=request.user, type=2).order_by('-date').first()
+    if last_balance is None and last_savings_balance is None:
+        messages.warning(request, 'No current balance has been recorded. '
+                                  'Please add at least one current balance record.')
+        make_calls = False
+    return render(request, 'my_finances/year_overview.html', context={'make_calls': make_calls})
+
+
+def get_summary_tiles(request):
     today = date.today()
-    total_income = Income.objects.\
-        filter(user=request.user, date__gte=last_balance.date, date__lte=today, repetitive=False)\
+    last_balance = Balance.objects.filter(user=request.user, type=1).order_by('-date').first()
+    if not last_balance:
+        return JsonResponse({'error': 'No current balance has been recorded. '
+                                      'Please add at least one current balance record.'})
+    # initialise totals with sums of non repetitive incomes and outcomes
+    total_income = Income.objects \
+        .filter(user=request.user, date__gt=last_balance.date, date__lte=today, repetitive=False) \
         .aggregate(total=Sum('value'))['total']
     total_income = 0 if total_income is None else total_income
-    total_outcome = Outcome.objects. \
-        filter(user=request.user, date__gte=last_balance.date, date__lte=today, repetitive=False) \
+    total_outcome = Outcome.objects \
+        .filter(user=request.user, date__gt=last_balance.date, date__lte=today, repetitive=False) \
         .aggregate(total=Sum('value'))['total']
     total_outcome = 0 if total_outcome is None else total_outcome
 
+    # updated totals with repetitive
     for income in Income.objects.filter(user=request.user, repetitive=True):
-        total_income += calculate_repetitive_total(income, last_balance, today)
+        total_income += calculate_repetitive_total(income, last_balance.date, today)
     for outcome in Outcome.objects.filter(user=request.user, repetitive=True):
-        total_outcome += calculate_repetitive_total(outcome, last_balance, today)
+        total_outcome += calculate_repetitive_total(outcome, last_balance.date, today)
 
-    context = {
-        'last_balance': last_balance,
-        'esitmated_balance': last_balance.value + total_income - total_outcome,
+    return JsonResponse({
+        'last_balance_value': last_balance.value,
+        'last_balance_date': last_balance.date,
+        'estimated_balance': last_balance.value + total_income - total_outcome,
         'total_income': total_income,
-        'total_outcome': total_outcome,
-    }
-    return render(request, 'my_finances/current_finances.html', context)
+        'total_outcome': total_outcome
+    })
 
 
-def finance_history(request):
-    return render(request, 'my_finances/finance_history.html')
+def get_year_chart(request):
+    balance_type = request.GET.get('balance_type')
+    if balance_type not in ['current', 'savings']:
+        return JsonResponse({'error', 'Please specify balance_type parameter to be either "current" or "savings".'})
+
+    today = date.today()
+    beginning_of_year = date(today.year, 1, 1)
+    end_of_year = date(today.year, 12, 28)
+    # Try to get last balance check before the beginning of this year
+    balance = Balance.objects.filter(user=request.user, type=1 if balance_type == 'current' else 2,
+                                     date__lte=beginning_of_year).order_by('-date').first()
+    if balance is None:
+        # If there's no balance check before the year - get this first one of this year
+        balance = Balance.objects.filter(user=request.user, type=1 if balance_type == 'current' else 2)\
+            .order_by('date').first()
+        if balance is None:
+            return JsonResponse({'error': 'No current balance has been recorded. '
+                                          'Please add at least one current balance record.'})
+
+    # Assuming we found balance - we start calculating from there
+    balance_checks = {}
+    income_per_day = {}
+    outcome_per_day = {}
+
+    for b in Balance.objects.filter(user=request.user, type=1 if balance_type == 'current' else 2,
+                                    date__gte=balance.date):
+        balance_checks[str(b.date)] = b.value
+
+    if balance_type == 'current':
+        for i in Income.objects.filter(user=request.user, date__gte=balance.date, repetitive=False):
+            income_per_day[str(i.date)] = i.value if str(i.date) not in income_per_day \
+                else income_per_day[str(i.date)] + i.value
+        for o in Outcome.objects.filter(user=request.user, date__gte=balance.date, repetitive=False):
+            outcome_per_day[str(o.date)] = o.value if str(o.date) not in outcome_per_day \
+                else outcome_per_day[str(o.date)] + o.value
+
+        for income in Income.objects.filter(user=request.user, repetitive=True):
+            calculate_repetitive_total(income, balance.date, end_of_year, income_per_day)
+        for outcome in Outcome.objects.filter(user=request.user, repetitive=True):
+            calculate_repetitive_total(outcome, balance.date, end_of_year, outcome_per_day)
+    else:
+        # If balance_type is savings, we want to only look at incomes and outcomes of type SAVINGS
+        # Also, income of type SAVINGS will act like an outcome for savings balance and vice versa
+        for i in Income.objects.filter(user=request.user, date__gte=balance.date, type=5, repetitive=False):
+            outcome_per_day[str(i.date)] = i.value if str(i.date) not in outcome_per_day \
+                else outcome_per_day[str(i.date)] + i.value
+        for o in Outcome.objects.filter(user=request.user, date__gte=balance.date, type=10, repetitive=False):
+            income_per_day[str(o.date)] = o.value if str(o.date) not in income_per_day \
+                else income_per_day[str(o.date)] + o.value
+
+        for income in Income.objects.filter(user=request.user, type=5, repetitive=True):
+            calculate_repetitive_total(income, balance.date, end_of_year, outcome_per_day)
+        for outcome in Outcome.objects.filter(user=request.user, type=10, repetitive=True):
+            calculate_repetitive_total(outcome, balance.date, end_of_year, income_per_day)
+
+    labels = []
+    data_estimated = []
+    data_balance_check = []
+    data_today = []
+    date_marker = balance.date
+    balance_on_marker_date = balance.value
+
+    if date_marker > beginning_of_year:
+        fill_date_marker = date(today.year, 1, 1)
+        while fill_date_marker < date_marker:
+            labels.append(str(fill_date_marker))
+            data_estimated.append(None)
+            data_balance_check.append(None)
+            data_today.append(None)
+            fill_date_marker += timedelta(days=1)
+    else:
+        while date_marker < beginning_of_year:
+            balance_on_marker_date += income_per_day.get(str(date_marker), 0)
+            balance_on_marker_date -= outcome_per_day.get(str(date_marker), 0)
+            date_marker += timedelta(days=1)
+
+    # Calculate and prepare balance per day for this year
+    while date_marker <= end_of_year:
+        labels.append(str(date_marker))
+        if str(date_marker) in balance_checks:
+            balance_on_marker_date = balance_checks[str(date_marker)]
+            data_balance_check.append(balance_checks[str(date_marker)])
+        else:
+            balance_on_marker_date += income_per_day.get(str(date_marker), 0)
+            balance_on_marker_date -= outcome_per_day.get(str(date_marker), 0)
+            data_balance_check.append(None)
+        data_estimated.append(balance_on_marker_date)
+        if date_marker == today:
+            data_today.append(balance_on_marker_date)
+        else:
+            data_today.append(None)
+        date_marker += timedelta(days=1)
+    return JsonResponse({'labels': labels, 'data_estimated': data_estimated, 'data_balance_check': data_balance_check,
+                         'data_today': data_today})
+
+
+def get_income_or_outcome_by_type(request):
+    get_what = request.GET.get('get_what')
+    summary_type = request.GET.get('summary_type')
+    if get_what is None or get_what not in ['income', 'outcome']:
+        return JsonResponse({'error': 'Please specify get_what parameter to be either "income" or "outcome".'})
+    if get_what == 'income':
+        obj = Income
+        obj_types = Income.ITypes
+    else:
+        obj = Outcome
+        obj_types = Outcome.OTypes
+
+    today = date.today()
+    if summary_type == 'current_period':
+        last_balance = Balance.objects.filter(user=request.user, type=1).order_by('-date').first()
+        if not last_balance:
+            return JsonResponse({'error': 'No current balance has been recorded. '
+                                          'Please add at least one current balance record.'})
+        start_date = last_balance.date
+        end_date = today
+    elif summary_type == 'year_overview':
+        start_date = date(today.year - 1, 12, 31)
+        end_date = date(today.year, 12, 31)
+    else:
+        return JsonResponse({'error': 'Please specify summary_type parameter to be either "current_period" or '
+                                      '"year_overview".'})
+
+    labels = []
+    data = []
+    for obj_type in obj_types.choices:
+        labels.append(obj_type[1])
+        total = obj.objects.filter(user=request.user, type=obj_type[0], date__gt=start_date,
+                                   date__lte=end_date, repetitive=False).aggregate(total=Sum('value'))['total']
+        total = 0 if total is None else total
+
+        for o in obj.objects.filter(user=request.user, type=obj_type[0], repetitive=True):
+            total += calculate_repetitive_total(o, start_date, end_date)
+        data.append(total)
+
+    return JsonResponse({'labels': labels, 'data': data})
